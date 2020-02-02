eaa45ac8a38156fffed5c3656a3f325a88096dc8
markajeena <55476784+markajeena@users.noreply.github.com>
2020-02-01 17:05:06 -0800

    Lab0.c
    added reading a file to the code

--- Lab0.c
+++ Lab0.c
@@ -1,6 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h> // For exit()
+void readFile(){
+FILE *fp;
+   char filename[100], c;
+   printf("Enter the filename to open \n");
+   scanf("%s", filename);
+
+   fp = fopen(filename, "r");
+   if(fp == NULL){
+         printf("Cant open file \n");
+            exit(0);
+
+      }
+   c = fgetc(fp);
+   while(c != EOF){
+    printf("%c",c);
+    c = fgetc(fp);
+   }
+    fclose(fp);
+
+}
 
 int main(){
+readFile();
 
 
 }
