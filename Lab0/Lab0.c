1bf48f6acf834ceaf51bdc9f31a5582cc09b9d8f
markajeena <55476784+markajeena@users.noreply.github.com>
2020-02-13 13:49:00 -0800

    FIX
    I have to fix many things like my read to fit the assignment standards and have to implement the nonprintable ASCII values. More changes to come to the logic behind the decimal conversion method. Also, I have to make sure to check that the input can function the way the professor wanted it to.

--- Lab0/Lab0.c
+++ Lab0/Lab0.c
@@ -4,6 +4,16 @@
 #include <string.h>
 #define bool int
 
+
+
+//Unprintable ASCII chars from 0-31, 127
+/* 
+    "NUL", "SOH", "STX", "ETX", "EOT", "ENQ", "ACK", "BEL",
+    " BS", "TAB", " LF", " VT", " FF", " CR", " SO", " SI",
+    "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN", "ETB",
+    "CAN", " EM", "SUB", "ESC", " FS", " GS", " RS", " US",
+    "space", "DEL"};  */
+
 int DecimalConversion(char *D){
       int decimal = 0, i, remainder;
       int cn = atoi(D);
