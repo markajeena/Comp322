8b2e205d0314f24d9cd5f381781b6a7158f72bb7
markajeena <55476784+markajeena@users.noreply.github.com>
2020-02-25 20:41:19 -0800

    Declare Variables
    just declaring variables and including packages I might use.

--- Lab1/lab1.c
+++ Lab1/lab1.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h> 
+#include <unistd.h>
 
 int main(int argc, char **argv){
-
+time_t start;
+time_t stop;
+pid_1 child_pid;
+  
 }
