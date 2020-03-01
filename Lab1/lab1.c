c61eb6497e44ca86840ccc6aae486288186fa7e8
markajeena <55476784+markajeena@users.noreply.github.com>
2020-02-29 16:14:32 -0800

    no internet :(
    I had no internet for the past couple of days, I finished this around Friday. I will tell you what I would've committed.
    1. Declaration of global variables. 
    2. set local variables
    3. check if process is in child or parent or neither, and print process ID for both parent and/or child
    4.work on the print statement
    5. system and user times w/ stop
    6.realized I suck and I added wait, basically it waits for a process in child or parent to wait before allowing others to go.(AKA sequential)
    7.finishing touches and organization
    8. will include a readme with more detailed commits later, and turn it in

--- Lab1/lab1.c
+++ Lab1/lab1.c
@@ -2,12 +2,54 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/types.h>
-#include <sys/wait.h> 
+#include <sys/wait.h>
+#include <sys/times.h>
 #include <unistd.h>
 
-int main(int argc, char **argv){
-time_t start;
-time_t stop;
-pid_1 child_pid;
-  
+//declare global variables
+   time_t start;
+   pid_t cpid;
+   time_t stop;
+   int status;
+   struct tms start_tms, end_tms;
+
+int main(){
+
+   cpid = fork();
+   pid_t wait;
+   time(&start);
+   times(&start_tms);
+   printf("START: %ld\n", start);//num of sec
+
+//check if is in child
+   if(cpid == 0){
+      printf("PPID: %d, ", getppid());//parent process ID
+         printf("PID: %d\n", getpid());//childs process ID
+   return 0;
+   }else if(cpid > 0){ //checks if in parent
+   wait = waitpid(cpid, &status, WUNTRACED);//waits for child to finish
+
+   printf("PPID: %d, PID: %d, CPID: %d, RETVAL: %d\n", getppid(), getpid(), wait, status);
+
+   times(&end_tms);
+
+   }
+
+   //if fork fails
+   else {
+      printf("FORK FAILED");
+      return 0;
+   }
+	//user time, system time
+   printf("USER: %ld," ,(end_tms.tms_utime - start_tms.tms_utime));
+   printf(" SYS: %ld\n", (end_tms.tms_stime - start_tms.tms_stime));
+	// user time of child, system time of child
+   printf("CUSER: %ld,",(end_tms.tms_cutime - start_tms.tms_cutime));
+   printf("CSYS: %ld\n", (end_tms.tms_cstime - start_tms.tms_cstime));
+
+   time(&stop);
+   printf("STOP: %ld\n", stop);
+
+
+   return 0;
 }
