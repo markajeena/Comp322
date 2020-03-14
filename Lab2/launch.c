71dbe591b6597f6a8f71b6c5747992d87dd72d7a
markajeena <55476784+markajeena@users.noreply.github.com>
2020-03-14 15:14:40 -0700

    Update launch.c
    added memory allocation, and fixed a bit more thing when arguments < 2, <= 1. fixing when the child pid == 0, to represent correct results when statements are invoked.

--- Lab2/launch.c
+++ Lab2/launch.c
@@ -1,4 +1,4 @@
-;/*Mark Ajina
+/*Mark Ajina
 Start date - 03/03/2020 
 End date - 
 Lab 2 - Launch Tube */
@@ -8,16 +8,34 @@ Lab 2 - Launch Tube */
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char** argv){
       int curr;
      pid_t cpid;
+      char* flocate;
+      
+      flocate = argv[1];
+      
+      char **args = (char** ) calloc(argc-1, sizeof(char*));
+      
+      for(int i = 0; i < argc - 1; i++){
+            args[i] = (char*) calloc(50, sizeof(char));
+      }
+      
+      if(argc < 2){
+            for(int i = 1; i < argc; i++)
+                  strcpy(argv[i-1], argv[i]);
+      }
+      cpid = fork();
       
       if(argc <= 1){
             fprintf(stderr, "ERROR: USAGE: %s NEEDS MORE ARGS\n", argv[0]);
        exit(EXIT_FAILURE);     
+            for(int i = 0; i < argc; i++)
+                  free(argv[i]);
       }
-      cpid = fork();
+      
 if(cpid == 0){
       
       printf("PPID: %d, ", getppid());//parent process ID
@@ -26,7 +44,7 @@ if(cpid == 0){
       
    }else if(cpid > 0){ //checks if in parent
       fprintf(stderr, "%s: $$ = %d\n", argv[1], getpid());
-      waitpid(cpid, &curr, WUNTRACED);
+      waitpid(cpid, &curr, 0);
       fprintf(stderr, "%s: $? = %d\n", argv[1],curr);
 
    }
