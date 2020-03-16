0566ea23ba66bfe2ca9f17531f22cf7fa32c6404
markajeena <55476784+markajeena@users.noreply.github.com>
2020-03-15 18:04:37 -0700

    Update tube.c
    added pipefd and fixed statement releasing an error statement if invoked

--- Lab2/tube.c
+++ Lab2/tube.c
@@ -6,16 +6,19 @@
 int main(int argc, char** argv){
 pid_t cpid1, cpid2;
 int check1, check2;
-
+int pipefd[2];
+  
+if(pipe(pipefed) == -1){
+perror("ERROR w/ Pipe);
+exit(EXIT_FAILURE);
+}
+        
 if(argc <= 1){
 fprintf(stderr, "ERROR: USAGE: %s NEEDS MORE ARGS\n, argv[0]);
 exit(EXIT_FAILURE);
 }
 
-if(argc == -1){
-fprintf(stderr, "ERROR: USAGE %s FAILED\n, argv[0]);
-exit(EXIT_FAILURE);
-}
+
 cpid1 = fork();
 cpid2 = fork();
 }
