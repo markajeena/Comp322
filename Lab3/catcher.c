ba1b1536ed9ebfa687c7cd4acee4e32b0d08a6ee
markajeena <55476784+markajeena@users.noreply.github.com>
2020-04-12 23:13:28 -0700

    Update catcher.c
    added libraries and declared local/global variables.

--- Lab3/catcher.c
+++ Lab3/catcher.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+#include <string.h>
+
+//Declare global variables
+static int terminateSig;
+static int counter;
+static char* SIGS[27] = {"HUP", "INT", "QUIT", "ILL", "TRAP",
+                         "ABRT", "BUS", "FPE", "KILL", "USR1",
+                         "SEGV", "USR2", "PIPE", "ALRM", "TERM", 
+                         "STKFLT", "CHLD", "CONT", "STOP", "TSTP",
+                         "TTIN", "TTOUT", "URG", "XCPU", "XFSZ",
+                         "VTALRM", "PROF", "WINCH"};
+
 
 int main(int argc, char** argv){
-  
- printf("Hello"); 
-  
+//local variables
+int catcher;
+  fprintf(stderr, "%s $$; %d\n"", argv[0], getpid());
+ 
+        
+          return 0;
 }
