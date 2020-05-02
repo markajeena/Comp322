df7f7419a13bb47ff0cd4eb5c917e7116c04ff16
markajeena <55476784+markajeena@users.noreply.github.com>
2020-05-02 07:11:29 -0700

    Update launch-philosophers.sh

--- Lab4/launch-philosophers.sh
+++ Lab4/launch-philosophers.sh
@@ -10,7 +10,26 @@ if [ $# -gt 2 ];
 then
 echo "Error: input less arguments";
 exit 1;
+fi
 
+#if there's no number of seats given
+if [ -z ${seats} && ${seats} -lt 0 ] ; 
+then
+echo "Enter a number of seats (must be positive).";
+exit 1;
+fi
+
+#if there's no position number given
+if [ -z ${position} &&  ${position} -lt 0 ] ; then
+echo "Enter a number for a philosopher(must be positve)";
+exit 1;
+fi
+
+
+#if there's less seats than philosophers/position given
+if [ ${seats} -lt ${position} ] ; then
+      echo "Error: not enough seats.";
+      exit 1;
 fi
 
 #creates a seat for every philosopher
