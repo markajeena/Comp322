192ba9441aaba53c872d04d5559908f231076f4d
markajeena <55476784+markajeena@users.noreply.github.com>
2020-05-02 21:37:05 -0700

    Update lab5.c
    added comments and cleaned up a little

--- Lab5/lab5.c
+++ Lab5/lab5.c
@@ -2,25 +2,29 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv){
+  //define local variables
+  //4kb = 4096 bytes
   const int size = 4096;
   unsigned int pageNum, address, offset;
   
+  //check if # is < than 2
   if(argc < 2){
    printf("Error: not enough number of arguments");
     
     return EXIT_FAILURE;
-  }
+  }//end if
   
+  //Convert value into integer
   address = atoi(argv[1]);
-  
+  //computation of page number and offset
   pageNum = address/size;
-  
   offset = address % size;
   
+  //Display address, page number, and offset
   printf("The address %d contains:\n", address);
   printf("Page Number = %d\n", pageNum);
   printf("Offset = %d\n", offset);
   
   return 0;
   
-}
+}//end main
