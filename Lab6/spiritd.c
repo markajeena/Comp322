f27948c3f8625ef2cf7109ba8a3dcd0189c219d8
markajeena <55476784+markajeena@users.noreply.github.com>
2020-05-04 18:05:57 -0700

    Update and rename lab6.c to spiritd.c

---
+++ Lab6/spiritd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv){
+  
+  
+  
+  return 0;
+  
+}
