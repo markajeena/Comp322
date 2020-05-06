489438b1c608e3f8fda75f71b8270973f24ed1a3
markajeena <55476784+markajeena@users.noreply.github.com>
2020-05-06 15:02:11 -0700

    Update spiritd.c

--- Lab6/spiritd.c
+++ Lab6/spiritd.c
@@ -14,11 +14,34 @@ pid_t pid2;
 pid_t dpid; //Daemon
 
 int main(int argc, char **argv){
-  pid_t ppid,
-  
+  pid_t ppid;
+  umask(0);
   ppid = fork();
+  int dev_null;
   
+  if(ppid < 0 || ppid > 0){
+    printf("Can't create a process")
+    return EXIT_FAILURE;
+    
+  }else if(ppid == 0){
+    setsid();
+    int check = chdir("/");
+    if(check == -1){
+      perror("cant change directories");
+      return EXIT_FAILURE;
+    
+    getrlimit(RLIMIT_NOFILE, &rlimit);
+        if (rlimit.rlim_max == RLIM_INFINITY)
+            rlimit.rlim_max = 1024;
+        for (unsigned int i = 0; i < rlimit.rlim_max; i++)
+            close(i);
+        dev_null = open("/dev/null", O_RDONLY);
+        dup2(dev_null,0);
+        dup2(dev_null,1);
+        dup2(dev_null,2);
+     }
   
+  }  
   return 0;
   
 }
