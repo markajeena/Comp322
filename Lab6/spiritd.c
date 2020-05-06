e9004793ae4328710387d23e68635d8e86cd4633
markajeena <55476784+markajeena@users.noreply.github.com>
2020-05-05 21:42:19 -0700

    Update spiritd.c

--- Lab6/spiritd.c
+++ Lab6/spiritd.c
@@ -4,6 +4,14 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/resource.h>
+#include <signal.h>
+#include <syslog.h>
+#include <limits.h>
+
+pid_t pid1;
+pid_t pid2;
+pid_t dpid; //Daemon
 
 int main(int argc, char **argv){
   pid_t ppid,
